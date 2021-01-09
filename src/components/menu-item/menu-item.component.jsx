dd606b6a2797d326256343222dee6494d034cab4
cwills02 <67972116+cwills02@users.noreply.github.com>
2021-01-09 16:54:10 -0600

    update our menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
     <div 
       className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
       <div
         className="background-image"
@@ -21,4 +23,7 @@ const MenuItem = ({title, imageUrl, size}) => {
   )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
+
+// withRouter is a HOC, which means its a function that takes a component as an argument and returns a new component with new properties
+// e.g., withRouter() gives us the history, match, and params properties from the react-router-dom library
