705d1ade3d18aa7580c19ba199c23ca2d8e1554b
cwills02 <67972116+cwills02@users.noreply.github.com>
2021-01-14 01:29:13 -0600

    implemented firebase utils, included ability to store authenticated users in our firestore database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -21,12 +21,32 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            }
+          })
+          // }, () => {
+          //     console.log(this.state);
+          //     // we have to call console.log(this.state) after we get back the async info from setState because setState is asynchronous, means there is a chance when we call that setState is not finished being called. Only way to call this.state correctly after setState is to pass a second function as an argument to the setState so we can log there
+          // })
+          console.log(this.state);
+        });
+      }
+      else {
+        this.setState({currentUser: userAuth});
+      }
     });
   }
+
+  // in the componentDidMount() we first check if a user is logged in, then if they exist in the database get back the userRef, but if they do not exist we create it (function is in the firebase.utils) and get back the userRef
+  // then we subscribe, listen to the userRef for changes and also get back the first state of the data, which is the snapShot data and id,set the state of the local app.js with the snapshot info, then if the user logs out we;ll make sure to set the currentUser to null
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,31 @@ const config = {
     messagingSenderId: "1068665681164",
     appId: "1:1068665681164:web:377c86dea5ea11af63c529",
     measurementId: "G-DLSCZJS99Y"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.log('error creating user', error.message);
+        }
+    }
+    return userRef;
 }
   
 firebase.initializeApp(config);
@@ -22,3 +47,17 @@ provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
+
+// test query example from firestore cloud database
+// import firebase from 'firebase/app';
+// import 'firebase/firestore';
+
+// const firestore = firebase.firestore();
+
+// firestore.collection('users').doc('KXKY696dcBv7emfVV5Rg').collection('cartItems').doc('1T5bXr2EGjdkrwhtkxl5');
+
+// alternative query for specific cartItem
+// firestore.doc('users/KXKY696dcBv7emfVV5Rg/cartItems/1T5bXr2EGjdkrwhtkxl5');
+
+// collection of cartItems
+// firebase.collection('/users/KXKY696dcBv7emfVV5Rg/cartItems');
